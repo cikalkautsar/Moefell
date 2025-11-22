2248548b0495820fc90814bcf3ba3ffa30cd106f
cikalkautsar <cikalkautsar@upi.edu>
2025-11-22 11:11:23 +0700

    bikin page doa

---
+++ app/doa.tsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Doa = () => {
+  const router = useRouter();
+
+  return (
+    <View>
+      <Text>Halaman doa</Text>
+    </View>
+  );
+};
+
+export default Doa;
